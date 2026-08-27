cc44887621dc1d3fa2dcacbe355f8a7898382741
Shayaan <syeda3@uw.edu>
2026-08-27 14:50:42 -0700

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0){
+            return false;
+        }
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        int left = 0;
+        int right = rows*columns-1;
+
+        while (left<=right) {
+            int midpoint = left + (right-left)/2;
+            int element = matrix[midpoint/columns][midpoint%columns];
+            if (element == target) {
+                return true;
+            } else if (element < target){
+                left = midpoint + 1;
+            } else if (element > target){
+                right = midpoint - 1;
+            }
+        }
+        return false;
+    }
+}
