90a9ef63b71951b9fba05385062f3a0ab5b9cd2e
Shayaan <syeda3@uw.edu>
2026-08-28 16:59:18 -0700

    Add: max-water-container - submission-5

---
+++ Data Structures & Algorithms/max-water-container/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int right = height.length - 1;
+        int left = 0;
+        int maxHeight = 0;
+        while (left<right){
+            if (height[left] < height[right]){
+                maxHeight = Math.max(maxHeight, height[left]* (right-left));
+                left++;
+            } else {
+                maxHeight = Math.max(maxHeight, height[right]*(right-left));
+                right--;
+            }
+        }
+        return maxHeight;
+    }
+}
